5ed120bc0337b84271a6fe1a8bc278db154f3dd6
Christine <cmbenson88@gmail.com>
2019-02-04 13:34:20 -0600

    Clean up RoomList.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
-import RoonList from './components/RoomList';
+import RoomList from './components/RoomList';
 
-<script src="https://www.gstatic.com/firebasejs/5.8.2/firebase.js"></script>
-  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyC0YBs_byzdaOmeXnqhgQ39Aw_V-YioWqY",
     authDomain: "feelin-chatty.firebaseapp.com",
@@ -20,22 +17,9 @@ import RoonList from './components/RoomList';
     render() {
       return (
         <div className="App">
-          <header>
-            <nav className="navbar">
-              <div className="container">
-                <div class="navbar" id="navbarNav">
-                  <ul className="navbar-nav">
-                    <li className="nav"><Link to='/'className="nav">Home</Link></li>
-                    <li className="nav"><Link to='/library'className="nav">Rooms</Link></li>
-                  </ul>
-                </div>
-             </div>
-            </nav>
-            <h1>Feelin Chatty</h1>
-          </header>
-          <main>
-            <Route path="/RoomList" component={RoomList} />
-          </main>
+          <RoomList
+            firebase={firebase}
+            />
         </div>
       );
     }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,25 +7,30 @@ import './App.css';
 class RoomList extends Component {
   constructor(props) {
     super(props);
+      this.state = {
+        rooms : [],
+    }
 
-    this.state = {
-      rooms: []
+    this.roomsRef = this.props.firebase.database().ref('rooms')
     };
-
-  this.roomsRef = this.props.firebase.database().ref('rooms');
 
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
-    const room = snapshot.val();
-    room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) })
+      const room = snapshot.val();
+      room.key = snapshot.key;
+    this.setState({ rooms: this.state.rooms.concat( room )});
+  });
+}
 
 render() {
   return(
-    <ul>
-    {this.state.rooms.map( (room, key) =>
-      <li>{room.key}</li>
-    }
-    </ul>
-  )
+    <section className={style.container}>
+      <h1>Room List</h1>
+      {this.state.rooms.map( (room =>
+        <h1>key={room.key}>{room.name}</h1>
+    )}
+    </section>
+  );
 }
+
+export default RoomList;
