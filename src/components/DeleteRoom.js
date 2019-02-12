c0c424407071633dd0c8a266491caf2d2a4f5662
Christine <cmbenson88@gmail.com>
2019-02-12 14:02:51 -0600

    Modal icon click working

--- src/components/DeleteRoom.js
+++ src/components/DeleteRoom.js
@@ -20,28 +20,34 @@ class DeleteRoom extends Component {
   }
   render(){
     return (
-    <div>
-    <i className="far fa-trash-alt" onClick={() => this.handleShow()}></i>
-    <div class="modal" tabIndex="-1" role="dialog" show={this.state.show} onHide={() => this.handleClose()}>
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Delete Room?</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
+  <div>
+
+    <i className="far fa-trash-alt" data-toggle="modal" data-target="#myModal" onClick={() => this.handleShow()}></i>
+    <div class="modal" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+
+          <div class="modal-header">
+           <h4 class="modal-title">Delete Room?</h4>
+           <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
         <div class="modal-body">
-          <p>Are you sure you want to delete <span className="font-weight-bold">{this.props.room.name}</span>?</p>
+        Are you sure you want to delete <span className="font-weight-bold">{this.props.room.name}?</span>
         </div>
+
+
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal" onClick={() =>this.handleClose()}>Cancel</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
           <button type="button" class="btn btn-primary" onClick={() => this.handleClose(this.props.room.name)}>Yes, Delete Room</button>
         </div>
+
       </div>
     </div>
   </div>
   </div>
+
   );
 }
 }
